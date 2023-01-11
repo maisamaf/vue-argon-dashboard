6b86fb531e845fe71e4d9850de45c247137929aa
Maisam Afshar <maisam.afshar@gmail.com>
2023-01-11 14:00:22 +0430

    update version & date.

--- src/assets/scss/argon.scss
+++ src/assets/scss/argon.scss
@@ -1,11 +1,11 @@
 /*!
 
 =========================================================
-* Vue Argon Dashboard - v2.0.1
+* Vue Argon Dashboard - v2.1.0
 =========================================================
 
 * Product Page: https://www.creative-tim.com/product/vue-argon-dashboard
-* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
 * Licensed under MIT (https://github.com/creativetimofficial/vue-argon-dashboard/blob/master/LICENSE.md)
 
 * Coded by Creative Tim
